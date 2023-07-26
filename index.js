14cff8e68b1191a455dd0f63248040ab0168d92c
Ghafran Abbas <ghafranabbas@gmail.com>
2023-07-26 09:25:31 -0400

    updates

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-var vitalchat = require('vitalchat');
+var _ = require("lodash");
+var vitalchat = require("vitalchat");
 var client = new vitalchat({
-    host: process.env.VITALCHAT_HOST,
-    key: process.env.VITALCHAT_KEY,
-    secret: process.env.VITALCHAT_SECRET
+  host: process.env.VITALCHAT_HOST,
+  key: process.env.VITALCHAT_KEY,
+  secret: process.env.VITALCHAT_SECRET,
 });
 
-client.on('event', (event) => {
-    console.log(event);
+client.on("event", (event) => {
+  console.log(event);
 });
 
-client.on('log', (log) => {
-    console.log(log);
+client.on("log", (log) => {
+  console.log(log);
 });
 
-client.on('error', (err) => {
-    console.log(err);
+client.on("error", (err) => {
+  console.log(err);
 });
 
 (async () => {
-    console.log('connecting to', process.env.VITALCHAT_HOST);
-    var devices = await client.devices();
-    if (devices.length === 0) {
-        console.log('no devices found');
-        return;
-    }
-    var data = await client.enter({
-		device: devices[0].device_id,
-		name: 'test caller',
-		device_description: 'John Smith'
-	});
-    console.log(data);
-    client.listen();
+  console.log("connecting to", process.env.VITALCHAT_HOST);
+  var devices = await client.devices();
+  if (devices.length === 0) {
+    console.log("no devices found");
+    return;
+  }
+  let device = _.find(devices, { status: "online" });
+  if (!device) {
+    console.log("no online device found");
+    return;
+  }
+  console.log(`creating call link to ${device.name}`);
+  var data = await client.enter({
+    device: device.device_id,
+    name: "test caller",
+    device_description: "John Smith",
+  });
+  console.log(data);
+  client.listen();
 })().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
